306bfca796060d3d53a2ec254597e88804311af5
Putrique <putrique.matheus@escola.pr.gov.br>
2024-11-25 20:42:34 -0300

    Create Móveis.js

---
+++ Móveis.js
@@ -0,0 +1,270 @@
+
+
+export const movies = [
+  {
+    title: "Turma da Mônica: Laços",
+    genre: "Comédia",
+    ageRating: "Livre",
+    synopsis: "O Floquinho desapareceu. Para encontrar seu cachorro de estimação, Cebolinha conta com os amigos Cascão, Mônica e Magali e, claro, um plano infalível.",
+    trailer: "https://youtu.be/Mjz_IqLtI58?si=wG4s-plx60VC-BbI "
+  },
+  {
+    title: "Matilda",
+    genre: "Comédia",
+    ageRating: 10,
+    synopsis: "Matilda é uma criança brilhante que cresceu ignorada pelos pais, a ponto de esquecerem de matriculá-la na escola. Quando a menina descobre que possui poderes mágicos e seu pai a manda estudar, ela precisa proteger os colegas da malvada diretora.",
+    trailer: "https://youtu.be/1UigDLytK5A?si=_SrUPZxtPTXF7dCM "
+  },
+  {
+    title: "É Fada",
+    genre: "Comédia",
+    ageRating: 12,
+    synopsis: "Geraldine, uma fada tagarela e atrapalhada, recebe a missão de ajudar a jovem Júlia, que tem dificuldade de lidar com os novos colegas e não acredita no mundo da magia.",
+    trailer: "https://youtu.be/s3m3GMC5gik?si=GZUbMBvJfS6UalWN "
+  },
+  {
+    title: "Uma Ideia de Você",
+    genre: "Comédia",
+    ageRating: 14,
+    synopsis: "Solène, uma mãe solteira de 40 anos embarca em um romance inesperado com Hayes Campbell, o vocalista de 24 anos da boy band August Moon, a mais popular do planeta.",
+    trailer: "https://youtu.be/ZeIGZdfmBhg?si=8YqXvhEFQiNFvgk4 "
+  },
+  {
+    title: "Todos Menos Você",
+    genre: "Comédia",
+    ageRating: 16,
+    synopsis: "Bea e Ben têm um primeiro encontro incrível, mas a atração inicial logo se torna ódio mútuo. Um casamento na Austrália força a aproximação dos dois, que decidem fingir um relacionamento para enganar a família e os amigos.",
+    trailer: "https://youtu.be/NrwlZ6lNVPo?si=lMO2b0o65a-47JIi "
+  },
+  {
+    title: "O Estranho Mundo de Jack",
+    genre: "Terror",
+    ageRating: "Livre",
+    synopsis: "Jack Skellington, o Rei das Abóboras, se cansa de fazer o Dia das Bruxas todos os anos e deixa os limites da cidade. Por acaso, acaba atravessando o portal do Natal, onde vê a alegria do espírito natalino. Ao retornar para a Cidade do Halloween, sem ter compreendido o que viu, ele começa a convencer os cidadãos a sequestrar o Papai Noel e fazerem seu próprio Natal. Apesar de sua leal namorada Sally ser contra, o Papai Noel é capturado e os fatos mostrarão que Sally estava certa o tempo todo.",
+    trailer: "https://youtu.be/E8o_8iKS6mk?si=jWehdryuBq_gI3-J "
+  },
+  {
+    title: "Coraline e o Mundo Secreto",
+    genre: "Terror",
+    ageRating: 10,
+    synopsis: "Enquanto explora sua nova casa à noite, a pequena Coraline descobre uma porta secreta que contém um mundo parecido com o dela, porém melhor em muitas maneiras. Todos têm botões no lugar dos olhos, os pais são carinhosos e os sonhos de Coraline viram realidade por lá. Ela se encanta com essa descoberta, mas logo percebe que segredos estranhos estão em ação: uma outra mãe e o resto de sua família tentam mantê-la eternamente neste mundo paralelo.",
+    trailer: "https://youtu.be/X3Pc9emMSxk?si=6dfD6ujAKKvcmT9F "
+  },
+  {
+    title: "A Família Addams 2",
+    genre: "Terror",
+    ageRating: 12,
+    synopsis: "Bebê. Porém os irmãos Wednesday e Pugsley não estão muito felizes com a novidade e tentam eliminar o bebê. Quando a babá Debbie Jellinsky aparece para manter as crianças na linha, sua presença leva a uma reviravolta inesperada.",
+    trailer: "https://youtu.be/_kRFIn6bNwQ?si=cTSMMt_lrOhppAfE "
+  },
+  {
+    title: "Os Fantasmas Ainda Se Divertem: Beetlejuice",
+    genre: "Terror",
+    ageRating: 14,
+    synopsis: "Retornamos à casa em Winter River, onde três gerações da família Deetz se unem após uma tragédia familiar inesperada.",
+    trailer: "https://youtu.be/CoZqL9N6Rx4?si=UsybSEsT3eDQ856e "
+  },
+  {
+    title: "Todo Mundo em Pânico",
+    genre: "Terror",
+    ageRating: 16,
+    synopsis: "Depois do assassinato de uma bela colega de classe, um grupo de adolescentes desorientados descobre que há um assassino entre eles. A heroína Cindy Campbell e a sua turma de amigos tentam se proteger do perigo, mas Gail Hailstorm, uma repórter irritante, simplesmente não os deixa em paz.",
+    trailer: "https://youtu.be/HTLPULt0eJ4?si=2kYxzYhjfs8UUnQT "
+  },
+  {
+    title: "Altas Aventuras",
+    genre: "Animação",
+    ageRating: "Livre",
+    synopsis: "Carl Fredricksen é um vendedor de balões que, aos 78 anos, está prestes a perder a casa em que sempre viveu com sua esposa, a falecida Ellie. Após um incidente, Carl é considerado uma ameaça pública e forçado a ser internado. Para evitar que isto aconteça, ele põe balões em sua casa, fazendo com que ela levante voo. Carl quer viajar para uma floresta na América do Sul, onde ele e Ellie sempre desejaram morar, mas descobre que um problema embarcou junto: Russell, um menino de 8 anos.",
+    trailer: "https://youtu.be/HWEW_qTLSEE?si=Jc-WjpeYJqz8dBc1 "
+  },
+  {
+    title: "Homem-Aranha: Através do Aranhaverso",
+    genre: "Animação",
+    ageRating: 10,
+    synopsis: "Depois de se reunir com Gwen Stacy, Homem-Aranha é jogado no multiverso. Lá, o super-herói aracnídeo encontra uma numerosa equipe encarregada de proteger sua própria existência.",
+    trailer: "https://youtu.be/uhkDkurK0Zg?si=Q14pVKdW610ANd9O "
+  },
+  {
+    title: "Liga da Justiça: Crise nas Infinitas Terras - Parte 2",
+    genre: "Animação",
+    ageRating: 12,
+    synopsis: "Um exército interminável de demônios sombrios, empenhados na destruição de todas as realidades, assola todas as Terras paralelas. A única coisa que se opõe a eles é a mais poderosa equipe de meta-humanos já reunida.",
+    trailer: "https://youtu.be/f91n6Jk2IX8?feature=shared "
+  },
+  {
+    title: "Liga da Justiça Sombria",
+    genre: "Animação",
+    ageRating: 14,
+    synopsis: "A Liga da Justiça encontra uma força sobrenatural contra a qual seus poderes parecem ser inúteis, e Batman reúne uma nova equipe formada por heróis míticos como o Monstro do Pântano, o Demônio e Constantine.",
+    trailer: "https://youtu.be/w5xyNvjcqCg?si=Xfx-JoFU9WPHM-JY "
+  },
+  {
+    title: "Injustice",
+    genre: "Animação",
+    ageRating: 16,
+    synopsis: "Em um mundo alternativo, o Coringa engana Superman para matar Lois Lane, fazendo com que o herói entre em um caminho de destruição.",
+    trailer: "https://youtu.be/T6zcYHdmSeA?si=yxWh4TDTW-sX_fGq "
+  },
+  {
+    title: "Os Incríveis",
+    genre: "Ação",
+    ageRating: "Livre",
+    synopsis: "Depois de o governo banir o uso de superpoderes, o Sr. Incrível vive de forma pacata com sua família, até que surge uma nova ameaça.",
+    trailer: "https://youtu.be/iqKEnKRDkds?si=B-1qP_iCMi_cI28 "
+  },
+  {
+    title: "Homem-Aranha no Aranhaverso",
+    genre: "Ação",
+    ageRating: 10,
+    synopsis: "Miles Morales descobre habilidades de aranha e conhece outras versões do herói de universos paralelos.",
+    trailer: "https://youtu.be/SS6ABPkfm” 
+export const movies = [
+  {
+    title: "Forrest Gump – O Contador de Histórias",
+    genre: "Drama",
+    ageRating: "Livre",
+    synopsis: "Forrest Gump, um homem com um QI abaixo da média, conta sua história de vida, repleta de eventos históricos. Ele se torna um herói de guerra, participa de eventos icônicos e vive uma história de amor complicada com sua amiga de infância, Jenny.",
+    trailer: "https://youtu.be/vDY_uZAaU7g?si=3KQOndEYG80MPTq6" 
+  },
+  {
+    title: "O Jogo da Imitação",
+    genre: "Drama",
+    ageRating: 10,
+    synopsis: "Durante a Segunda Guerra Mundial, o matemático e criptoanalista Alan Turing trabalha para decifrar o código Enigma, usado pelos nazistas. Em meio a preconceitos e segredos, ele precisa enfrentar desafios pessoais e profissionais que em sua vida e seu trabalho.",
+    trailer: "https://youtu.be/cXoKh1nxj1E?si=8frx9CX5DV1bQ8ny" 
+  },
+  {
+    title: "A Procura da Felicidade",
+    genre: "Drama",
+    ageRating: 12,
+    synopsis: "Baseado em uma história real, o filme acompanha Chris Gardner, um vendedor que enfrenta dificuldades financeiras extremas e luta para cuidar de seu filho pequeno enquanto busca uma vida melhor. Chris consegue uma oportunidade em um programa de estágio em uma grande corretora de ações, mas, sem receber salário, ele enfrenta uma jornada de sacrifício e superação.",
+    trailer: "https://youtu.be/yHgiEADa5M8?si=i323CjS2m5Rw6ugH" 
+  },
+  {
+    title: "Um Sonho de Liberdade",
+    genre: "Drama",
+    ageRating: 14,
+    synopsis: "Condenado injustamente pelo assassinato de sua esposa e do amante dela, Andy Dufresne é enviado à prisão de Shawshank, onde forma uma amizade com outro prisioneiro, Red. Ao longo dos anos, Andy usa seu conhecimento em finanças para ajudar outros presos e o sistema prisional, enquanto elabora um plano de fuga.",
+    trailer: "https://youtu.be/Y22NgkErOAk?si=7R-Me0Q58JZYsQ8p" 
+  },
+  {
+    title: "Clube da Luta",
+    genre: "Drama",
+    ageRating: 16,
+    synopsis: "Um homem insatisfeito com sua vida de escritório e sua insônia encontra um vendedor de sabão carismático. Juntos, eles formam um clube secreto onde homens podem liberar suas frustrações por meio de lutas, o que acaba desencadeando uma série de eventos violentos e transformadores.",
+    trailer: "https://youtu.be/Fs0-4NLSO2Y?si=NtEP0AJ4SNYL7rf-"
+  }
+];
+export const movies = [
+  {
+    title: "O Sexto Sentido",
+    genre: "Suspense",
+    ageRating: "Livre",
+    synopsis: "Um garoto vê e se comunica com espíritos, e busca ajuda com um psicólogo infantil, que, ao longo do tempo, passa a acreditar na habilidade do menino. Enquanto tenta ajudá-lo a entender sua habilidade, o psicólogo também descobre segredos sobre si mesmo.",
+    trailer: ""
+  },
+  {
+    title: "Os Outros",
+    genre: "Suspense",
+    ageRating: 10,
+    synopsis: "Uma mulher que vive em uma mansão isolada com seus dois filhos começa a suspeitar que sua casa é assombrada. Seus filhos, sensíveis à luz, vivem em quartos escuros, o que torna o ambiente ainda mais misterioso e assustador.",
+    trailer: "https://youtu.be/iZmV5gOwHRI?si=jfTgLwU0B36Y6VmZ"
+  },
+  {
+    title: "O Silêncio dos Inocentes",
+    genre: "Suspense",
+    ageRating: 12,
+    synopsis: "Uma jovem agente do FBI tenta capturar um assassino em série, e para isso pede ajuda ao Dr. Hannibal Lecter, um psiquiatra brilhante e canibal, que está preso. O suspense aumenta conforme ela se envolve no jogo psicológico dele.",
+    trailer: "https://youtu.be/UQmpyBY7Xes?si=b9aPq2s-j5E-EmAL"
+  },
+  {
+    title: "Ilha do Medo",
+    genre: "Suspense",
+    ageRating: 14,
+    synopsis: "Em 1954, um detetive e seu parceiro vão a um hospital psiquiátrico em uma ilha remota para investigar o desaparecimento de uma paciente. Conforme investigam, o detetive começa a desconfiar que coisas estranhas estão sendo ocultadas e passa a questionar sua própria sanidade.",
+    trailer: "https://youtu.be/F1u7XjE7wCs?si=LGSuBRi6q6VnbYsz"
+  },
+  {
+    title: "Corra!",
+    genre: "Suspense",
+    ageRating: 16,
+    synopsis: "Um jovem negro visita a família de sua namorada branca em uma área isolada e percebe que as coisas não são o que parecem. Ele descobre um segredo perturbador que envolve desaparecimentos misteriosos e estranhos comportamentos da família.",
+    trailer: "https://youtu.be/mDGV5UucTu8?si=jhZfeSqlxrds_Qh1"
+  }
+];
+export const movies = [
+  {
+    title: "E.T. - O Extraterrestre",
+    genre: "Ficção Científica",
+    ageRating: "Livre",
+    synopsis: "Um garoto faz amizade com um extraterrestre perdido na Terra e tenta ajudá-lo a voltar para casa, enquanto precisam evitar que o governo descubra sua presença. A amizade entre eles resulta em uma história emocionante e cheia de aventuras.",
+    trailer: "https://youtu.be/qYAETtIIClk?si=9awIo5Jg5sgIsO8N"
+  },
+  {
+    title: "De Volta para o Futuro",
+    genre: "Ficção Científica",
+    ageRating: 10,
+    synopsis: "Um adolescente é acidentalmente transportado para o passado em uma máquina do tempo construída por seu amigo cientista excêntrico. No passado, ele encontra seus pais e precisa garantir que eles se apaixonem para que ele continue a existir.",
+    trailer: "https://youtu.be/DYFBYfzA90E?si=5FkSpvm94KZfFdDI"
+  },
+  {
+    title: "Interestelar",
+    genre: "Ficção Científica",
+    ageRating: 12,
+    synopsis: "Em um futuro próximo, a Terra está em colapso, e um grupo de astronautas embarcam em uma jornada por um buraco de minhoca para encontrar um novo planeta habitável para a humanidade. A missão explora limites científicos e emocionais enquanto lidam com o tempo e a gravidade.",
+    trailer: "https://youtu.be/yMnauR8K2Zc?si=IWurLilPDor0ax_-"
+  },
+  {
+    title: "A Origem",
+    genre: "Ficção Científica",
+    ageRating: 14,
+    synopsis: "Um especialista em invadir sonhos é contratado para realizar uma 'inserção' - plantar uma ideia na mente de alguém. Em uma missão complexa, ele e sua equipe precisam navegar por camadas de sonhos, enfrentando perigos psicológicos que desafiam sua percepção da realidade.",
+    trailer: "https://youtu.be/R_VX0e0PX90?si=6c-VFcLJXlRC20A6"
+  },
+  {
+    title: "Blade Runner 2049",
+    genre: "Ficção Científica",
+    ageRating: 16,
+    synopsis: "Em um futuro distópico, um caçador de androides descobre um segredo que pode mudar a humanidade e o leva a buscar um antigo caçador de replicantes. Essa busca coloca em questão o que significa ser humano e aborda dilemas éticos e existenciais.",
+    trailer: "https://youtu.be/xGwe7D0RKWc?si=_KIpQJe-yTBibkUJ"
+  }
+];
+export const movies = [
+  {
+    title: "Jumanji",
+    genre: "Aventura",
+    ageRating: "Livre",
+    synopsis: "Dois irmãos descobrem um jogo de tabuleiro mágico chamado Jumanji, que libera criaturas e perigos da selva em sua casa a cada jogada. Para encerrar o caos, eles precisam terminar o jogo, enfrentando diversos desafios emocionantes e misteriosos.",
+    trailer: "https://youtu.be/tnpV3q8Q3gg?si=f2psyY-PBi29Yfkj"
+  },
+  {
+    title: "Jurassic Park",
+    genre: "Aventura",
+    ageRating: 10,
+    synopsis: "Em uma ilha remota, um bilionário cria um parque temático com dinossauros clonados a partir de DNA antigo. Mas quando o sistema de segurança falha, os visitantes e funcionários do parque precisam lutar para sobreviver em meio a perigosas criaturas pré-históricas.",
+    trailer: "https://youtu.be/tazmO2FNYEk?si=7WsvBMbZWWpY09vh"
+  },
+  {
+    title: "Piratas do Caribe: A Maldição do Pérola Negra",
+    genre: "Aventura",
+    ageRating: 12,
+    synopsis: "O excêntrico pirata Jack Sparrow une forças com um jovem ferreiro para resgatar a filha do governador e recuperar seu navio, o Pérola Negra. Juntos, eles enfrentam uma tripulação amaldiçoada que se torna imortal sob a luz do luar.",
+    trailer: "https://youtu.be/2PBYQDDkubM?si=IR0oN2Ev8a0eXfAs"
+  },
+  {
+    title: "O Senhor dos Anéis: A Sociedade do Anel",
+    genre: "Aventura",
+    ageRating: 14,
+    synopsis: "Frodo, um hobbit, recebe a missão de destruir um poderoso anel que pode trazer a escuridão ao mundo. Ele parte em uma jornada épica com uma sociedade de heróis, enfrentando inimigos sombrios e superando desafios para proteger a Terra Média.",
+    trailer: "https://youtu.be/0i86oM1nHjM?si=zIpZMpnNhUvLxWPl"
+  },
+  {
+    title: "Mad Max: Estrada da Fúria",
+    genre: "Aventura",
+    ageRating: 16,
+    synopsis: "Em um mundo pós-apocalíptico, Max se une a uma guerreira chamada Furiosa para escapar de um tirano cruel e sua gangue. Em uma perseguição frenética pelo deserto, eles lutam para sobreviver e ajudar outros a escapar da opressão.",
+    trailer: "https://youtu.be/TdJ7DMwAukU?si=j5bx199SOQS9cCW-"
+  }
+];
+
